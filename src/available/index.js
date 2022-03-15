baeb40e5a33c20cefc60431f66d910a77647433b
Kai Blin <kblin@biosustain.dtu.dk>
2022-03-15 09:25:41 +0100

    available: Provide a list of valid locus evidences
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

--- src/available/index.js
+++ src/available/index.js
@@ -1,4 +1,18 @@
 import express from 'express';
 const router = express.Router();
 
+
+router.get("/locus/evidences", (req, res) => {
+    res.json({
+        evidences: [
+            "Sequence-based prediction",
+            "Gene expression correlated with compound production",
+            "Knock-out studies",
+            "Enzymatic assays",
+            "Heterologous expression",
+        ],
+    })
+    return;
+});
+
 export default router;
